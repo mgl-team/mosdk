1f05abfcbc3fc872015f24f59ab503d15555c7b8
amjil <amjil@protonmail.com>
2021-05-08 17:11:12 +0800

    add model folder

---
+++ src/app/model/doc.cljs
@@ -0,0 +1,40 @@
+(ns app.model.doc)
+
+(def cur-id 0)
+
+(def doc 
+  {:pages []
+   :settings {}
+   :width 100
+   :height 100}
+  
+  )
+
+(def ldoc
+  {:contents []})
+
+
+(def flowobject
+  {:type :image
+   :content :obj
+   :text ""
+   :w 0
+   :h 0
+   :width 10
+   :height 10})
+
+(def paragraph 
+  {:id 1
+   :page-num 1
+   :type :paragraph
+   :str ""
+   ;; split long text to fit doc height
+   :text []})
+
+(def doc-content
+  {:str ""
+   :width 10
+   :height 10
+   :page 1
+   :line 2
+   :style {}})
