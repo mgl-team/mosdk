e1c80ac6c7073f73efd7fbf96ae82bf6a38ad805
amjil <amjil@protonmail.com>
2021-05-14 17:54:12 +0800

    model

--- src/app/model/doc.cljs
+++ src/app/model/doc.cljs
@@ -1,43 +1,91 @@
 (ns app.model.doc)
 
-(def cur-id 0)
+(def font
+  {:name          :default
+   :original-size 10
+   :scale         1
+   :size          10})
 
-(def doc 
+(def text-style
+  {:bold             false
+   :italic           false
+   :strike-through   false
+   :underline        false
+   :font             font
+   :color            "#000000"
+   :vert-align       :baseline ;; :superScript :subscript
+   :align            :default ;; left right center justified
+   :background-color "#000000"
+   :high-light       "#000000"})
+   
+(def bound 
+  {:x 0
+   :y 0
+   :w 0
+   :h 0})
+
+(def flowobject
+  {:bound bound
+   :type  :default
+   :w     0 
+   :h     0})
+
+(def element
+  {:type    :text ;; :flow :table
+   :content nil
+   :style   {}
+   :pos     0
+   :width   0
+   :height  0})
+
+(def cell-style 
+  {})
+
+(def cell 
+  {:style   cell-style
+   :content nil
+   :w       0
+   :h       0})
+
+(def table
+  {:rows    0
+   :columns 0
+   :cells   []})
+
+(def range
+  {:x 0
+   :y 0
+   :w 0
+   :h 0
+   :elements []})
+
+(def column
+  :ranges [])
+
+(def indent 
+  {:level 0
+   :type :default})
+
+(def paragraph
+  {:bound        bound
+   :space        1.15
+   :space-after  3.5
+   :space-before 0
+   :indent       indent
+   :columns      []})
+
+
+(def content 
+  {:type :para ;; table image math
+   :obj  nil})
+
+(def page 
+  {:bound    {}
+   :contents []
+   :flows    []})
+
+(def doc
   {:pages []
    :settings {}
    :width 100
-   :height 100}
-  
-  )
-
-(def ldoc
-  {:contents []})
-
-
-(def flowobject
-  {:type :image
-   :content :obj
-   :text ""
-   :w 0
-   :h 0
-   :width 10
-   :height 10})
-
-(def paragraph 
-  {:id 1
-   :page-num 1
-   :type :paragraph
-   :str ""
-   ;; split long text to fit doc height
-   :text []})
-
-(def para-line
-  {:content []})
-
-(def para-str
-  {:str ""
-   :width 10
-   :height 10
-   :page 1
-   :line 2
-   :style {}})
+   :height 100})
--- src/app/model/style.cljs
+++ src/app/model/style.cljs
@@ -1,30 +1,30 @@
 (ns app.model.style)
 
-(def font-style
-  {:name          nil
-   :original-size 10
-   :scale         1
-   :size          10})
-
-(def text-style
-  {:bold             false
-   :italic           false
-   :strike-through   false
-   :underline        false
-   :font             {}
-   :color            "#000000"
-   :vert-align       :baseline ;; :superScript :subscript
-   :align            :default ;; left right center justified
-   :spacing          0
-   :background-color nil
-   :high-light       nil})
-
-(def line
-  {:spacing 1.15
-   :contents []})
-
-(def paragraph-style
-  {:spacing 0.035})
-
 ;;https://edu.gcfglobal.org/en/word2016/line-and-paragraph-spacing/1/
 
+;;
+;; Document
+;; 	Curpos
+;; 	Selection
+;; 	Action
+;; 	Pages
+
+;; History
+;; 	index
+;; 	Items []
+;; Clipboard
+;; Events
+;; Cursor
+;;   x y
+;; Search
+;; Header
+;; Footer
+;; Hit
+;; Styles
+;; ContextMenu
+;; TextInput
+;; TextMeasurer
+;; Scroll
+;; Zoom
+;; Rules
+;; Watermark
